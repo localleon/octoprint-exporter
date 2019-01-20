caa9e3a32ecb606eda5ada7ed7ce08f238bb390a
localleon <leon.rauschenbach@web.de>
2019-01-20 12:07:20 +0100

    Working on the Exporter Structure and the json parsing of the API

---
+++ octoprint-exporter/collector.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//Define a struct for you collector that contains pointers
+//to prometheus descriptors for each metric you wish to expose.
+//Note you can also include fields of other types if they provide utility
+//but we just won't be exposing them as metrics.
+type JobCollector struct {
+	estimatedPrintTime *prometheus.Desc
+}
+
+//You must create a constructor for you collector that
+//initializes every descriptor and returns a pointer to the collector
+func newJobCollector() *JobCollector {
+	return &JobCollector{
+		estimatedPrintTime: prometheus.NewDesc("octoprint_estimated_print_time",
+			"Returns the estimated print time of the current job",
+			nil, nil,
+		),
+	}
+}
+
+//Each and every collector must implement the Describe function.
+//It essentially writes all descriptors to the prometheus desc channel.
+func (collector *JobCollector) Describe(ch chan<- *prometheus.Desc) {
+
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.estimatedPrintTime
+}
+
+//Collect implements required collect function for all promehteus collectors
+func (collector *JobCollector) Collect(ch chan<- prometheus.Metric) {
+	//Implement logic here to determine proper metric value to return to prometheus
+	//for each descriptor or call other functions that do so.
+
+	//Write latest value for each metric in the prometheus metric channel.
+	//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+	ch <- prometheus.MustNewConstMetric(collector.estimatedPrintTime, prometheus.GaugeValue, apiJobProgress())
+}
