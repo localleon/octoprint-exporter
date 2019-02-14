3e8425ad7d0148921163aa3fcc786e5b6af1a765
localleon <leon.rauschenbach@web.de>
2019-02-14 17:42:13 +0100

    Added Tempreature Metrics about tool0 and bed

--- octoprint-exporter/collector.go
+++ octoprint-exporter/collector.go
@@ -11,6 +11,11 @@ type JobCollector struct {
 	printTime          *prometheus.Desc
 	estimatedPrintTime *prometheus.Desc
 	status             *prometheus.Desc
+	sdStatus           *prometheus.Desc
+	extruderTemp       *prometheus.Desc
+	extruderTempTarget *prometheus.Desc
+	bedTemp            *prometheus.Desc
+	bedTempTarget      *prometheus.Desc
 }
 
 //initializes every descriptor and returns a pointer to the collector
@@ -20,7 +25,6 @@ func newJobCollector() *JobCollector {
 			"Returns the estimated print time of the current job",
 			nil, nil,
 		),
-
 		progress: prometheus.NewDesc("octoprint_progress",
 			"Progress State of the Current Job",
 			nil, nil,
@@ -37,6 +41,26 @@ func newJobCollector() *JobCollector {
 			"estimatedPrintTime from Octoprint",
 			nil, nil,
 		),
+		sdStatus: prometheus.NewDesc("octoprint_sd_status",
+			"Is an SD-Card connected to the Printer ? ",
+			nil, nil,
+		),
+		extruderTemp: prometheus.NewDesc("octoprint_temp_extruder",
+			"Temperature of Extruder from Octoprint",
+			nil, nil,
+		),
+		extruderTempTarget: prometheus.NewDesc("octoprint_temp_extruder_target",
+			"Target-Temperature of Extruder from Octoprint",
+			nil, nil,
+		),
+		bedTemp: prometheus.NewDesc("octoprint_temp_heatbed",
+			"Temperature of Heatbed from Octoprint",
+			nil, nil,
+		),
+		bedTempTarget: prometheus.NewDesc("octoprint_temp_heatbed_target",
+			"Target-Temperature of Heatbed from Octoprint",
+			nil, nil,
+		),
 	}
 }
 
@@ -47,33 +71,51 @@ func (collector *JobCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.printTime
 	ch <- collector.estimatedPrintTime
 	ch <- collector.status
+	ch <- collector.sdStatus
+	ch <- collector.extruderTemp
+	ch <- collector.extruderTempTarget
+	ch <- collector.bedTemp
+	ch <- collector.bedTempTarget
 }
 
 //Collect implements required collect function for all promehteus collectors
 func (collector *JobCollector) Collect(ch chan<- prometheus.Metric) {
 	// Get Infos about the current Job from the Octoprint-API
-	infos := apiGetJobInfo()
-
-	switch infos.State {
+	jobInfos := apiGetJobInfo()
+	switch jobInfos.State {
 	case "Printing from SD":
 		ch <- prometheus.MustNewConstMetric(collector.status, prometheus.GaugeValue, 2)
 		// Update Printing Metrics
-		ch <- prometheus.MustNewConstMetric(collector.estimatedPrintTime, prometheus.GaugeValue, float64(infos.Job.EstimatedPrintTime))
-		ch <- prometheus.MustNewConstMetric(collector.printTime, prometheus.GaugeValue, float64(infos.Progress.PrintTime))
-		ch <- prometheus.MustNewConstMetric(collector.printTimeLeft, prometheus.GaugeValue, float64(infos.Progress.PrintTimeLeft))
-		ch <- prometheus.MustNewConstMetric(collector.progress, prometheus.GaugeValue, infos.Progress.Completion)
+		ch <- prometheus.MustNewConstMetric(collector.estimatedPrintTime, prometheus.GaugeValue, float64(jobInfos.Job.EstimatedPrintTime))
+		ch <- prometheus.MustNewConstMetric(collector.printTime, prometheus.GaugeValue, float64(jobInfos.Progress.PrintTime))
+		ch <- prometheus.MustNewConstMetric(collector.printTimeLeft, prometheus.GaugeValue, float64(jobInfos.Progress.PrintTimeLeft))
+		ch <- prometheus.MustNewConstMetric(collector.progress, prometheus.GaugeValue, jobInfos.Progress.Completion)
 	case "Printing":
 		ch <- prometheus.MustNewConstMetric(collector.status, prometheus.GaugeValue, 3)
 		// Update Printing Metrics
 		// Update Printing Metrics
-		ch <- prometheus.MustNewConstMetric(collector.estimatedPrintTime, prometheus.GaugeValue, float64(infos.Job.EstimatedPrintTime))
-		ch <- prometheus.MustNewConstMetric(collector.printTime, prometheus.GaugeValue, float64(infos.Progress.PrintTime))
-		ch <- prometheus.MustNewConstMetric(collector.printTimeLeft, prometheus.GaugeValue, float64(infos.Progress.PrintTimeLeft))
-		ch <- prometheus.MustNewConstMetric(collector.progress, prometheus.GaugeValue, infos.Progress.Completion)
+		ch <- prometheus.MustNewConstMetric(collector.estimatedPrintTime, prometheus.GaugeValue, float64(jobInfos.Job.EstimatedPrintTime))
+		ch <- prometheus.MustNewConstMetric(collector.printTime, prometheus.GaugeValue, float64(jobInfos.Progress.PrintTime))
+		ch <- prometheus.MustNewConstMetric(collector.printTimeLeft, prometheus.GaugeValue, float64(jobInfos.Progress.PrintTimeLeft))
+		ch <- prometheus.MustNewConstMetric(collector.progress, prometheus.GaugeValue, jobInfos.Progress.Completion)
 	case "Operational":
 		ch <- prometheus.MustNewConstMetric(collector.status, prometheus.GaugeValue, 1)
 	default:
 		ch <- prometheus.MustNewConstMetric(collector.status, prometheus.GaugeValue, 0)
 	}
+	// Get Infos about tempreature and sd karte
+	printerInfos := apiGetPrinterInfo()
+	ch <- prometheus.MustNewConstMetric(collector.sdStatus, prometheus.GaugeValue, boolToBin(printerInfos.Sd.Ready))
+	ch <- prometheus.MustNewConstMetric(collector.extruderTemp, prometheus.GaugeValue, printerInfos.Temperature.Tool0.Actual)
+	ch <- prometheus.MustNewConstMetric(collector.extruderTempTarget, prometheus.GaugeValue, printerInfos.Temperature.Tool0.Target)
+	ch <- prometheus.MustNewConstMetric(collector.bedTemp, prometheus.GaugeValue, printerInfos.Temperature.Bed.Actual)
+	ch <- prometheus.MustNewConstMetric(collector.bedTempTarget, prometheus.GaugeValue, printerInfos.Temperature.Bed.Target)
+}
 
+func boolToBin(a bool) float64 {
+	if a {
+		return 1
+	} else {
+		return 0
+	}
 }
